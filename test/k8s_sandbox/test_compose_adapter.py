e092ce6123a2b48e059570cea105b350507257f2
Craig.Walton <Craig.Walton@dsit.gov.uk>
2025-02-24 12:11:15 +0000

    Add tests

--- test/k8s_sandbox/test_compose_adapter.py
+++ test/k8s_sandbox/test_compose_adapter.py
@@ -18,3 +18,61 @@ def test_converter(resources) -> None:
     actual = yaml.dump(result, sort_keys=False)
 
     assert actual == expected
+
+
+def tmp_compose_file(contents: str, tmp_path) -> Path:
+    compose_path = tmp_path / "compose.yaml"
+    compose_path.write_text(contents)
+    return compose_path
+
+
+def test_converts_mem_limit(tmp_path) -> None:
+    compose_path = tmp_compose_file(
+        """
+services:
+  my-service:
+    mem_limit: 1G
+""",
+        tmp_path,
+    )
+
+    result = convert(compose_path)
+
+    assert result["services"]["my-service"]["resources"]["limits"]["memory"] == "1Gi"
+
+
+def test_converts_deploy(tmp_path) -> None:
+    compose_path = tmp_compose_file(
+        """
+services:
+  my-service:
+    deploy:
+      resources:
+        limits:
+          memory: 1G
+""",
+        tmp_path,
+    )
+
+    result = convert(compose_path)
+
+    assert result["services"]["my-service"]["resources"]["limits"]["memory"] == "1Gi"
+
+
+def test_ignores_mem_limit_when_deploy_present(tmp_path) -> None:
+    compose_path = tmp_compose_file(
+        """
+services:
+  my-service:
+    mem_limit: 1G
+    deploy:
+      resources:
+        limits:
+          memory: 2G
+    """,
+        tmp_path,
+    )
+
+    result = convert(compose_path)
+
+    assert result["services"]["my-service"]["resources"]["limits"]["memory"] == "2Gi"
