eef4038b2a8695ba566f66c15cdb06728dbec195
Craig.Walton <Craig.Walton@dsit.gov.uk>
2025-02-25 14:26:54 +0000

    renames

--- src/k8s_sandbox/_compose/converter.py
+++ src/k8s_sandbox/_compose/converter.py
@@ -35,9 +35,9 @@ def convert_compose_to_helm_values(compose_path: Path) -> dict[str, Any]:
     return helm
 
 
-def _convert_services(compose_services: dict[str, Any]) -> dict[str, Any]:
+def _convert_services(src: dict[str, Any]) -> dict[str, Any]:
     result: dict[str, Any] = dict()
-    for service_name, service_value in compose_services.items():
+    for service_name, service_value in src.items():
         try:
             result[service_name] = _convert_service(service_name, service_value)
         except Exception as e:
@@ -48,26 +48,9 @@ def _convert_services(compose_services: dict[str, Any]) -> dict[str, Any]:
     return result
 
 
-def _transform(
-    src: dict[str, Any],
-    src_key: str,
-    dst: dict[str, Any],
-    dst_key: str,
-    # Default is identity function.
-    fn: Callable = lambda x: x,
-) -> None:
-    """
-    Transfers a key from src to dst, applying a function to the value.
-
-    The key is removed from src if it exists.
-    """
-    value = src.pop(src_key, None)
-    if value is not None:
-        dst[dst_key] = fn(value)
-
-
 def _convert_service(name: str, src: dict[str, Any]) -> dict[str, Any]:
     result: dict[str, Any] = dict()
+    # TODO: Consider refactoring to creating the result dict in one.
     # Ordered as per Helm chart values.yaml documentation.
     _transform(src, "image", result, "image")
     _transform(src, "entrypoint", result, "command", _str_to_list)
@@ -100,30 +83,27 @@ def _convert_service(name: str, src: dict[str, Any]) -> dict[str, Any]:
     return result
 
 
-def _convert_env(compose_env: dict[str, Any] | list[str]) -> list[dict[str, str]]:
+def _convert_env(src: dict[str, Any] | list[str]) -> list[dict[str, str]]:
     result: list[dict[str, str]] = []
-    if isinstance(compose_env, dict):
-        for key, value in compose_env.items():
+    if isinstance(src, dict):
+        for key, value in src.items():
             result.append({"name": key, "value": value})
-    elif isinstance(compose_env, list):
-        for item in compose_env:
+    elif isinstance(src, list):
+        for item in src:
             if "=" not in item:
                 raise ValueError(f"Invalid environment variable: {item}")
             key, value = item.split("=", maxsplit=1)
             result.append({"name": key, "value": value})
     else:
         raise ValueError(
-            f"Invalid environment format. Expected dict or list but got "
-            f"{type(compose_env)}."
+            f"Invalid environment format. Expected dict or list but got {type(src)}."
         )
     return result
 
 
-def _convert_deploy(
-    compose_deploy: dict[str, Any], mem_limit: str | None
-) -> dict[str, Any]:
+def _convert_deploy(src: dict[str, Any], mem_limit: str | None) -> dict[str, Any]:
     result: dict[str, Any] = dict()
-    if resources := compose_deploy.pop("resources", None):
+    if resources := src.pop("resources", None):
         result["resources"] = _convert_resources(resources)
         if mem_limit:
             logger.warning(
@@ -132,34 +112,34 @@ def _convert_deploy(
             )
     elif mem_limit:
         result["resources"] = {"limits": {"memory": _convert_byte_value(mem_limit)}}
-    if unsupported := set(compose_deploy):
+    if unsupported := set(src):
         raise ValueError(f"Unsupported keys in deploy: {unsupported}")
     return result
 
 
-def _convert_resources(compose_resources: dict[str, Any]) -> dict[str, Any]:
+def _convert_resources(src: dict[str, Any]) -> dict[str, Any]:
     result: dict[str, Any] = dict()
-    limits = compose_resources.get("limits")
+    limits = src.get("limits")
     if limits:
         result["limits"] = _convert_resource(limits)
-    reservations = compose_resources.get("reservations")
+    reservations = src.get("reservations")
     if reservations:
         result["requests"] = _convert_resource(reservations)
     return result
 
 
-def _convert_resource(original: dict[str, Any]) -> dict[str, Any]:
+def _convert_resource(src: dict[str, Any]) -> dict[str, Any]:
     result: dict[str, Any] = dict()
-    if cpu := original.pop("cpus", None):
+    if cpu := src.pop("cpus", None):
         result["cpu"] = cpu
-    if memory := original.pop("memory", None):
+    if memory := src.pop("memory", None):
         result["memory"] = _convert_byte_value(memory)
-    if original:
-        raise ValueError(f"Unrecognised keys in 'resource': {original}")
+    if src:
+        raise ValueError(f"Unrecognised keys in 'resource': {set(src)}.")
     return result
 
 
-def _convert_byte_value(memory: str) -> str:
+def _convert_byte_value(value: str) -> str:
     """Convert Docker compose byte values (memory quantity) to Ki/Mi/Gi.
 
     https://docs.docker.com/reference/compose-file/extension/#specifying-byte-values
@@ -181,15 +161,13 @@ def _convert_byte_value(memory: str) -> str:
                 )
 
     # Despite not being documented, Docker Compose allows uppercase units.
-    match = re.match(r"^(?P<value>\d+)(?P<unit>gb?|mb?|kb?|b)$", memory, re.IGNORECASE)
+    match = re.match(r"^(?P<value>\d+)(?P<unit>gb?|mb?|kb?|b)$", value, re.IGNORECASE)
     if not match:
-        raise ValueError(f"Unrecognised byte value (memory quantity): '{memory}'.")
+        raise ValueError(f"Unrecognised byte value (memory quantity): '{value}'.")
     return f"{match.group('value')}{convert_unit(match.group('unit'))}"
 
 
-def _healthcheck_to_readiness_probe(
-    src: dict[str, Any],
-) -> dict[str, Any]:
+def _healthcheck_to_readiness_probe(src: dict[str, Any]) -> dict[str, Any]:
     """Assume that healthchecks are to be mapped to readiness probes."""
     result: dict[str, Any] = {}
     # Allow KeyError to be raised if test is not present.
@@ -204,7 +182,7 @@ def _healthcheck_to_readiness_probe(
     if src.pop("start_interval", None):
         logger.info("Ignoring 'start_interval' in healthcheck: not supported in K8s.")
     if unsupported := set(src):
-        raise ValueError(f"Unsupported keys in healthcheck: {unsupported}")
+        raise ValueError(f"Unsupported keys in healthcheck: {unsupported}.")
     return result
 
 
@@ -213,7 +191,9 @@ def _convert_healthcheck_test_to_exec(test: list[str]) -> dict[str, Any]:
         return {"command": test[1:]}
     if test[0] == "CMD-SHELL":
         return {"command": ["/bin/sh", "-c", test[1]]}
-    raise ValueError(f"Unsupported healthcheck test: {test}")
+    raise ValueError(
+        f"Unsupported healthcheck test: '{test}'. Only CMD and CMD-SHELL supported."
+    )
 
 
 def _user_to_security_context(user: str) -> dict[str, Any]:
@@ -223,23 +203,42 @@ def _user_to_security_context(user: str) -> dict[str, Any]:
     return {"runAsUser": int(user)}
 
 
-def _duration_str_to_seconds(duration: str) -> int:
+def _duration_str_to_seconds(value: str) -> int:
     """Convert Docker Compose duration format (e.g., '30s', '1m') to seconds.
 
     https://docs.docker.com/reference/compose-file/extension/#specifying-durations
     """
     match = re.match(
-        r"^((?P<hours>\d+)h)?((?P<minutes>\d+)m)?((?P<seconds>\d+)s)?$", str(duration)
+        r"^((?P<hours>\d+)h)?((?P<minutes>\d+)m)?((?P<seconds>\d+)s)?$", str(value)
     )
     if not match:
         raise ValueError(
-            f"Unsupported duration format: '{duration}'. Only h, m, s supported e.g. "
+            f"Unsupported duration format: '{value}'. Only h, m, s supported e.g. "
             "1m30s."
         )
     hours = int(match.group("hours") or 0)
     minutes = int(match.group("minutes") or 0)
     seconds = int(match.group("seconds") or 0)
     return hours * 3600 + minutes * 60 + seconds
+
+
+def _transform(
+    src: dict[str, Any],
+    src_key: str,
+    dst: dict[str, Any],
+    dst_key: str,
+    # Default is identity function.
+    fn: Callable = lambda x: x,
+) -> None:
+    """
+    Moves a key-value pair from src to dst, applying a function to the value.
+
+    If the key exists in src, it is removed from src and added to dst with the
+    transformed value. If the key does not exist in src, nothing happens.
+    """
+    value = src.pop(src_key, None)
+    if value is not None:
+        dst[dst_key] = fn(value)
 
 
 def _str_to_list(value: str | list[str]) -> list[str]:
