9a62f86000562dc7341649e20de8db5ee7e3f2fe
rasmusfaber <r.faber+copilot@elsevier.com>
2025-05-14 15:57:15 +0200

    Default user (#97)
    * Support for default user
    
    * Tests
    
    * ruff
    
    * Refactor configuration
    
    * ruff. Test environment refactor.
    
    * Add default user (as per Rasmus PR).
    
    * Improve docstrings.
    
    * Add refactored tests,
    
    * Inline variable
    
    * Ruff
    
    * Move test changes out
    
    * Ruff
    
    * Reintroduce improved test code
    
    * Remove unnecessary parenthesises
    
    ---------
    
    Co-authored-by: Craig.Walton <Craig.Walton@dsit.gov.uk>

--- test/k8s_sandbox/utils.py
+++ test/k8s_sandbox/utils.py
@@ -1,34 +1,38 @@
 from contextlib import asynccontextmanager
 from pathlib import Path
-from typing import AsyncGenerator
+from typing import AsyncGenerator, cast
 
-from k8s_sandbox._helm import Release, StaticValuesSource
-from k8s_sandbox._sandbox_environment import K8sSandboxEnvironment
+from k8s_sandbox._sandbox_environment import (
+    K8sSandboxEnvironment,
+    K8sSandboxEnvironmentConfig,
+)
 
 
 @asynccontextmanager
 async def install_sandbox_environments(
-    task_name: str, values_filename: str | None, context_name: str | None = None
+    task_name: str,
+    values_filename: str | None,
+    context_name: str | None = None,
+    default_user: str | None = None,
 ) -> AsyncGenerator[dict[str, K8sSandboxEnvironment], None]:
     values_path = (
         Path(__file__).parent / "resources" / values_filename
         if values_filename
         else None
     )
-    values_source = StaticValuesSource(values_path)
-    release = Release(
-        task_name=task_name,
-        chart_path=None,
-        values_source=values_source,
-        context_name=context_name,
+    config = K8sSandboxEnvironmentConfig(
+        values=values_path, context=context_name, default_user=default_user
     )
     try:
-        await release.install()
-        pods = await release.get_sandbox_pods()
-        sandbox_envs = {
-            pod_name: K8sSandboxEnvironment(release, pod)
-            for pod_name, pod in pods.items()
-        }
-        yield sandbox_envs
+        envs = cast(
+            dict[str, K8sSandboxEnvironment],
+            await K8sSandboxEnvironment.sample_init(task_name, config, {}),
+        )
+        yield envs
     finally:
-        await release.uninstall(quiet=True)
+        try:
+            sandbox = next(iter(envs.values()))
+            await sandbox.release.uninstall(quiet=True)
+        except UnboundLocalError:
+            # Release wasn't successfully installed.
+            pass
