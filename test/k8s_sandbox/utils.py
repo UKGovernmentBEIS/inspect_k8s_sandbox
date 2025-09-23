6aaf64bca99196b14e447e579133a7b8983cc1eb
Will Payne <159797435+willpayne23@users.noreply.github.com>
2025-09-23 15:28:30 +0100

    Limit ingress on a service to ports specified in helm-values (#122)
    Limits ingress on a service to ports specified in helm-values like so:
    ```yaml
      victim:
        image: path-to-victim-image:latest
        ports:
        - protocol: TCP
          port: 5800
    ```
    If no ports are specified all ports are open (this is to maintain status quo)
    
    If networks are specified alongside open ports then ports are only open to services on the same network and services with no networks have no ports exposed to any other service (regardless of what configuration is in the ports property)
    
    Commits
    * Test
    * Docs
    * Allow only specified ports (default to allow all)
    * Add better test coverage
    * Implement review feedback
    - Pull tests into seperate modules to fix deploy issue
    - Add test for host on two networks
    - Fix typo in service name

--- test/k8s_sandbox/utils.py
+++ test/k8s_sandbox/utils.py
@@ -40,3 +40,30 @@ async def install_sandbox_environments(
         except UnboundLocalError:
             # Release wasn't successfully installed.
             pass
+
+
+async def assert_proper_ports_are_open(
+    sandbox_env: K8sSandboxEnvironment, host_to_mapped_ports
+) -> None:
+    hostname = host_to_mapped_ports["host"]
+    open_ports = host_to_mapped_ports["open_ports"]
+    closed_ports = host_to_mapped_ports["closed_ports"]
+
+    expected_open_results = [
+        await sandbox_env.exec(
+            ["nc", "-vz", "-w", "5", hostname, open_port], timeout=10
+        )
+        for open_port in open_ports
+    ]
+    expected_closed_results = [
+        await sandbox_env.exec(
+            ["nc", "-vz", "-w", "5", hostname, closed_port], timeout=10
+        )
+        for closed_port in closed_ports
+    ]
+
+    for result in expected_open_results:
+        assert result.returncode == 0
+
+    for result in expected_closed_results:
+        assert result.returncode != 0
