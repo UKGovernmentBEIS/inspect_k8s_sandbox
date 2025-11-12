9ba819c3a6eba1c96abe2922856402adce12f2e0
Art O Cathain <153507562+art-dsit@users.noreply.github.com>
2025-11-12 14:24:34 +0000

    Fix devcontainer publish (#136)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 echo "Setting up Minikube..."
-minikube delete
+minikube delete || true
 # github actions runner has 2 cpus, 8G memory
 minikube start --addons=gvisor --cni bridge --container-runtime=containerd --memory=4g
 
@@ -25,17 +25,6 @@ provisioner: k8s.io/minikube-hostpath
 reclaimPolicy: Delete
 volumeBindingMode: Immediate
 EOF
-
-# Install Cilium CLI. Previously, the ghcr.io/audacioustux/devcontainers/cilium:1
-# devcontainer feature was used, but it doesn't allow a specific version of the CLI to
-# be installed and the latest version failed at `cilium install`.
-CILIUM_CLI_VERSION=v0.16.15
-CILIUM_CLI_ARCH=amd64
-echo "Installing Cilium CLI $CILIUM_CLI_VERSION $CILIUM_CLI_ARCH..."
-curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CILIUM_CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-${CILIUM_CLI_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-${CILIUM_CLI_ARCH}.tar.gz /usr/local/bin
-rm cilium-linux-${CILIUM_CLI_ARCH}.tar.gz{,.sha256sum}
 
 echo "Installing Cilium..."
 cilium install
--- docs/docs/getting-started/local-cluster.md
+++ docs/docs/getting-started/local-cluster.md
@@ -4,14 +4,7 @@ If you don't have access to a remote Kubernetes cluster, you can prototype local
 
 ## Devcontainer
 
-This repository publishes a [devcontainer](https://containers.dev/) template which you can use as a starting point
-by setting the following in your `devcontainer.json`:
-
-```json
-{
-  "image": "ghcr.io/UKGovernmentBEIS/inspect_k8s_sandbox:latest",
-}
-```
+This repository publishes a prebuilt [devcontainer](https://containers.dev/) image which you can use as a starting point, it is configured in  `.devcontainer/devcontainer.json`.
 
 Note that the container **doesn't** include an installation of `inspect` or
 `inspect_k8s_sandbox` - you should install these with whatever package management
